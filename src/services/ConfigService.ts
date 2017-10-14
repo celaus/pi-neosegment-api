5b15d1254bc01c9fe93ad990b347fbe72368dbdc
Claus Matzinger <claus.matzinger+kb@gmail.com>
2017-10-14 22:34:35 +0200

    removed more errors

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -18,25 +18,11 @@ import { TomlReader } from '@sgarciac/bombadil';
 
 export class ConfigService {
 
-    private defaultImageWidth: number = 1920;
-    private defaultImageHeight: number = 1080;
-    private defaultTimeout: number = 10; // milliseconds
-    private defaultInterval: number = 60000; // milliseconds
     private defaultPort: number = 3000;
     private defaultTopics = ["topic"];
     private defaultBrokerAddress = 'localhost'
     private defaultBrokerPort = 1883
-    private defaultRole = 'role'
-    private defaultName = 'agent'
-
-    private get_resolution(config: any): Array<number> {
-        let resolution = [this.defaultImageWidth, this.defaultImageHeight];
-        if (config['timelapse']['resolution'] instanceof Array && config['timelapse']['resolution'].length === 2) {
-            resolution = config['timelapse']['resolution'];
-        }
-        return resolution;
-    }
-
+    
     /**
      * Parses a string configuration into an `Configuration` interface.
      * @param rawConfig TOML configuration to read from.
@@ -48,9 +34,6 @@ export class ConfigService {
             throw new Error(`Unable to read configuration: ${reader.errors ? reader.errors : "Invalid TOML found"}`);
         }
         const config = reader.result;
-        const resolution = this.get_resolution(config);
-        const imageWidth = resolution[0];
-        const imageHeight = resolution[1];
         return {
             http: {
                 port: config['http']['port'] || this.defaultPort,
